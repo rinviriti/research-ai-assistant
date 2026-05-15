8c004e6298b87324f90368eb429f4fa43781c66a
Rinvi Jaman Riti <155306221+rinviriti@users.noreply.github.com>
2026-05-15 22:32:28 +0600

    add centralized app color constants

---
+++ frontend/app/lib/core/constants/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const Color background = Color(0xFF0F172A);
+  static const Color card = Color(0xFF1E293B);
+  static const Color primary = Colors.blueAccent;
+  static const Color textPrimary = Colors.white;
+  static const Color textSecondary = Colors.white70;
+  static const Color danger = Colors.redAccent;
+}
